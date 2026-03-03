87c9341186eb5525b10f34d3af5b6ba7dd26445f
amandiv233-sudo <amandiv233@gmail.com>
2026-03-03 09:00:42 +0530

    Add ESLint config to ignore build errors
    Added ESLint configuration to ignore errors during builds.

--- plantsswaad/apps/web/next.config.mjs
+++ plantsswaad/apps/web/next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'unsplash.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'drive.google.com',
-            }
-        ],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
